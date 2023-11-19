01026ca74bb2cb9da3a19d178e59d40b5bae3a26
bomkyu <a01090762806@gmail.com>
2023-11-19 19:01:55 +0900

    _ADD 첫번째 커밋

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='visual'>
+        
+      </div>
+      <div className='content'>
+        asdasdasdasdasdasd
+      </div>
+    </div>
+  );
+}
+
+export default App;
